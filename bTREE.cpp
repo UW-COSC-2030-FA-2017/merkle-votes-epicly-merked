b63a4b22f1f18e0731929bef1e144f84027dd8fe
kearhar1 <31860986+kearhar1@users.noreply.github.com>
2017-12-01 12:22:42 -0700

    Add files via upload

--- bTREE.cpp
+++ bTREE.cpp
@@ -1,43 +1,233 @@
 #include "bTREE.h"
 
+#include <queue>
+
+using namespace std;
+
+
 
 //look at descriptions in pMT.h for guidance on what you might need for these function to actually do
+
 bTREE::bTREE()
+
 {
+    tree == NULL;
+
+    int h = 0;
+
+    nodeCounter = 0;
+
 }
+
+
 
 bTREE::~bTREE()
+
 {
+    delete(tree);
+
 }
+
+
 
 int bTREE::dataInserted()
+
 {
+    return dataInserted();
+
 }
 
+
+
 int bTREE::numberOfNodes()
+
 {
+    int num = 0;
+
+    return num;
+}
+
+void bTREE::postOrder(std::vector<short> & traversal, const treeNode * subtree)
+{
+    if(subtree != NULL)
+    {
+        postOrder(traversal, subtree->left);
+
+        postOrder(traversal, subtree->right);
+
+        traversal.push_back(subtree->entry);
+    }
 }
 
 int bTREE::insert(string data, int time)
+
 {
+    treeNode * n = new treeNode;
+
+    n->data = data;
+
+    n->time = time;
+
+    n->left = NULL;
+
+    n->right = NULL;
+
+    if(tree == NULL)
+    {
+        nodeCounter++;
+        tree = n;
+    }
+
+    treeNode * temp = queueHelper.front();
+
+    if(!temp->left)
+    {
+        temp->left = n;
+
+        queueHelper.push(n);
+    }
+    else
+    {
+        temp->right = n;
+
+        queueHelper.push(n);
+
+        queueHelper.pop();
+    }
+
+    return nodeCounter;
+
 }
 
-int bTREE::find(string)
+
+
+int bTREE::find(string string1)
+
 {
+    return find(string1);
 }
 
-string bTREE::locate()
+
+
+string bTREE::locate(string a, string)
+
 {
+    string none = " ";
+    return locate(a, none);
 }
 
-friend bool bTREE::operator ==(const bTREE& lhs, const bTREE& rhs)
+
+
+bool operator ==(const bTREE& lhs, const bTREE& rhs)
+
 {
+    if (lhs.tree == NULL && rhs.tree == NULL) return true;
+
+    if(lhs.tree == NULL || rhs.tree == NULL) return false;
+
+    if(lhs.tree->data.compare(rhs.tree->data) == 0 && lhs.tree->time == rhs.tree->time)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 }
 
-friend bool bTREE::operator !=(const bTREE& lhs, const bTREE& rhs)
+
+
+bool operator !=(const bTREE& lhs, const bTREE& rhs)
 {
+    if(lhs.tree == NULL && rhs.tree == NULL) return false;
+
+    if(lhs.tree == NULL || rhs.root == NULL) return true;
+
+    if(lhs.tree->data.compare(rhs.root->data) == 0 && lhs.tree->time == rhs.tree->time)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
 }
 
-friend std::ostream& bTREE::operator <<(std::ostream& out, const bTREE& p)
+
+
+ostream & operator <<(std::ostream& out, const bTREE& p)
 {
+    p.print(out);
+    return out;
+}
+
+void bTREE::print(ostream & outfile) const
+{
+    if (tree == NULL)
+    {
+        outfile << "-" << endl;
+    }
+    else
+    {
+        displayLeft(outfile, tree->leftpntr, "   ");
+
+        oufile << "---" << tree->data << endl;
+
+        displayRight(outfile, tree->rightpntr, "   ");
+    }
+
+}
+void bTREE::displayLeft(ostream & outfile, treeNode * subtree, std::string pre) const)
+{
+    if(subtree == NULL)
+    {
+
+        outfile << pre + "                    /" << endl;
+    }
+    else
+    {
+        displayLeft(outfile, subtree->leftpntr, pre + "                ");
+
+        outfile << pre + "           /---" << subtree->data << endl;
+
+        displayRight(outfile, subtree->rightpntr, pre + "          |   ");
+    }
+
+}
+
+void bTREE::displayRight(ostream & outfile, treeNode * subtree, std::string pre) const)
+{
+    if(subtree == NULL)
+    {
+
+        outfile << pre + "                    \\" << endl;
+    }
+    else
+    {
+        displayLeft(outfile, subtree->leftpntr, pre + "                ");
+
+        outfile << pre + "           \\---" << subtree->data << endl;
+
+        displayRight(outfile, subtree->rightpntr, pre + "             ");
+    }
+
+}
+void bTREE::destroy(treeNode * & subtree)
+{
+    if(subtree != NULL)
+    {
+        destroy(subtree->leftpntr);
+        destroy(subtree->rightpntr);
+        delete subtree;
+        subtree = NULL;
+    }
+}
+bTREE::treeNode* bTREE::getTree()
+
+{
+
+    return tree;
+
 }
--- bTREE.h
+++ bTREE.h
@@ -1,37 +1,108 @@
 #pragma once
+
 #include <string>
+
+#include <math.h>
+
+#include <queue>
+
 using namespace std;
 
+#ifndef BTREE_H
+
+#define BTREE_H
+
 class bTREE
+
 {
+
     struct treeNode{
+
         string data;
+
         int time;
+
+        bool isleaf;
+
+        string h;
+
+        short entry;
+
+        treeNode * leftpntr;
+
+        treeNode * rightpntr;
+
     };
-    
+
+    queue<treeNode*> queueHelper;
+
+
+
 private:
+
     //some data structure to hold your treeNodes together ...
+
     //DATASTUCTURE treeNodes tree;
+
     //any helper private variables you need
-    
+    int nodeCount;
+
+    void destroy(treeNode * & subtree);
+
+    treeNode * tree;
+
+    treeNode* left;
+
+    treeNode* right;
+
+    void displayLeft(ostream & outfile, treeNode  * subtree, string) const;
+
+    void displayRight(ostream & outfile, treeNode * subtree, string) const;
+
+
+
+
+
 public:
+
     bTREE();
+
     ~bTREE();
-    
+
+
+
     int dataInserted();
+
     int numberOfNodes();
-    
+
+    treeNode * getTree();
+
     bool insert(string, int);
-    
-    bool find(string);
-    
+
+
+
+    bool find(string string1);
+
+    void print(std::ostream& ) const;
+
     string locate(string);
-    
-    
+
+    static void postOrder(std::vector<short> & traversal, const treeNode *subtree);
+
+
+
+
+
     friend bool operator==(const bTREE& lhs, const bTREE& rhs);
+
     friend bool operator!=(const bTREE& lhs, const bTREE& rhs);
 
+
+
     friend std::ostream& operator<<(std::ostream& out, const bTREE& p);
-    
+
+
+
 };
+
 
