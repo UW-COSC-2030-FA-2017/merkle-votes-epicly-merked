f761b46edb165423d5ff418a50956a9a48d49ec9
kearhar1 <31860986+kearhar1@users.noreply.github.com>
2017-12-01 13:49:03 -0700

    Update vote.cpp

--- vote.cpp
+++ vote.cpp
@@ -1,9 +1,57 @@
 #include <stdio.h>
+
 #include "pMT.h"
+
+#include "bTREE.h"
+
+#include <iostream>
+
+#include <fstream>
+
+#include <string>
+
+#include <sstream>
+
 using namespace std;
 
+
+
 int main(int argc, char **argv)
+
 {
-	printf("hello world\n");
+
+	pMT tester(1);
+
+	string data, time;
+
+	ifstream myfile ("my_test.txt");
+	if (myfile.is_open())
+    {
+        while ( myfile >> data >> time)
+
+      {
+
+      	istringstream buffer(time);
+
+      	int t = 0;
+
+      	buffer >> t;
+
+        cout << tester.insert(data, t) << endl;
+
+      }
+
+      myfile.close();
+
+    }
+
+    else cout << "error opening file";
+
+
+
+    cout << tester;
+    }
+
 	return 0;
+
 }
